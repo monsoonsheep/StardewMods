86843a260bd06cc2b389dde94f1aa2a4e445f6cf
Monsoon <mansoor.shk35@gmail.com>
2025-05-02 13:46:40 +0500

    Add WrongForecast proj

---
+++ WrongForecast/Mod.cs
@@ -0,0 +1,37 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using HarmonyLib;
+global using Microsoft.Xna.Framework;
+global using StardewModdingAPI;
+global using StardewModdingAPI.Events;
+global using StardewValley;
+global using StardewMods.Common;
+using StardewMods.WrongForecast.Framework.Services;
+
+namespace StardewMods.WrongForecast;
+
+public class Mod : StardewModdingAPI.Mod
+{
+    internal static Mod Instance = null!;
+
+    internal static Harmony Harmony { get; private set; } = null!;
+    internal static IModEvents Events { get; private set; } = null!;
+    public Mod()
+        => Instance = this;
+
+    public override void Entry(IModHelper helper)
+    {
+        Log.Monitor = base.Monitor;
+        Harmony = new Harmony(base.ModManifest.UniqueID);
+        Events = this.Helper.Events;
+        this.Helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+    }
+
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+
+    }
+}
